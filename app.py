cfb25d3d9be52bcdc222666f1e801e158a2239b7
2023008193 <148780320+2023008193@users.noreply.github.com>
2023-10-23 14:09:25 +0000

    atualizar

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import panda as pd
 
+st.set_page_config('Daniel-ECOP06','https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png')
+
+st.title('Pagina de Teste ECOP06')
+
+esportes=pd.read_csv(
+    'https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv',
+    encoding='latin-1')
+
+st.dataframe(esportes)
